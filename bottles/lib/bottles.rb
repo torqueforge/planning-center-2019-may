6f070cc7eebe208e1a78ce0c2efc67034d410563
Sandi Metz <sandi@sandimetz.com>
2019-05-02 06:35:36 -0700

    polymorphic bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,51 +1,82 @@
 class Bottles
-      def song
-        verses(99, 0)
-      end
+  def song
+    verses(99, 0)
+  end
 
-      def verses(upper, lower)
-        upper.downto(lower).map { |i| verse(i) }.join("\n")
-      end
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
 
-      def verse(number)
-        case number
-        when 0
-          "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n#{action(number)}, 99 #{container(number)} of beer on the wall.\n"
-        else
-          "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n#{action(number)}, #{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
-        end
-      end
+  def verse(number)
+    bottle_number = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
 
-      def quantity(number)
-        if number == 0
-          "no more"
-        else
-          number.to_s
-        end
-      end
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, "+
+    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+  end
 
-      def container(number)
-        if number == 1
-          "bottle"
-         else
-          "bottles"
-         end
-      end
+  def bottle_number_for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
+  end
+end
 
-      def action(number)
-        if number == 0
-          "Go to the store and buy some more"
-        else
-          "Take #{pronoun(number)} down and pass it around"
-        end
-      end
 
-      def pronoun(number)
-        if number == 1
-          "it"
-        else
-          "one"
-        end
-      end
+class BottleNumber
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
 
-    end
+  def quantity
+    number.to_s
+  end
+
+  def container
+    "bottles"
+  end
+
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def successor
+    number - 1
+  end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    99
+  end
+end
+
+class BottleNumber1 < BottleNumber
+  def container
+    "bottle"
+  end
+
+  def pronoun
+    "it"
+  end
+end
